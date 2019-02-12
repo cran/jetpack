b190ae6665367b051ded8e487275d7b1e8563ecd
Andrew Kane <andrew@chartkick.com>
2019-02-12 10:33:32 +0000

    version 0.4.2

---
+++ R/init.R
@@ -0,0 +1,45 @@
+#' Set up Jetpack
+#'
+#' @export
+#' @examples \dontrun{
+#'
+#' jetpack::init()
+#' }
+init <- function() {
+  sandbox({
+    if (!file.exists("DESCRIPTION")) {
+      write("Package: app", file="DESCRIPTION")
+    }
+
+    initRprofile()
+
+    setupEnv()
+
+    if (!interactive()) {
+      success("Run 'jetpack add <package>' to add packages!")
+    } else {
+      success("Run 'jetpack::add(package)' to add packages!")
+      enablePackrat()
+      loadNamespace("jetpack", lib.loc=getDefaultLibPaths())
+    }
+    invisible()
+  })
+}
+
+initRprofile <- function() {
+  rprofile <- file.exists(".Rprofile")
+  if (!rprofile || !any(grepl("jetpack", readLines(".Rprofile")))) {
+    str <- "if (requireNamespace(\"jetpack\", quietly=TRUE)) {
+  jetpack::load()
+} else {
+  message(\"Install Jetpack to use a virtual environment for this project\")
+}"
+
+    if (rprofile) {
+      # space it out
+      str <- paste0("\n", str)
+    }
+
+    write(str, file=".Rprofile", append=TRUE)
+  }
+}
